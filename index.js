a2f8a5516502536ac0c759b67bb56a5e3100c81d
Devon Walsh <devonwalsh@gmail.com>
2021-03-17 14:23:09 -0400

    commit tree e60c3861059cdb9261f7e4557b764267df318e5c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching (driverNames, matchString) {
+    return driverNames.filter(name => name.toLowerCase() === matchString.toLowerCase())
+}
+
+function fuzzyMatch (driverNames, matchString) {
+    return driverNames.filter(name => name.startsWith(matchString))
+}
+
+function matchName (driverNames, matchString) {
+    return driverNames.filter(element => element.name === matchString)
+}
